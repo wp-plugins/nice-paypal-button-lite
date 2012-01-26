5ecd3d31930a6a9cb121c57897c0e29b72c3e854
mjetpax <mjetpax@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-26 18:11:45 +0000

    Adding version 1.00 of the Nice PayPal Button Lite plugin. This is the first commit.
    git-svn-id: https://plugins.svn.wordpress.org/nice-paypal-button-lite/trunk@495729 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ nicePayPalButtonLite.php
@@ -0,0 +1,380 @@
+<?php 
+/**
+ * @package Nice PayPal Button Lite
+ */
+/*
+Plugin Name: Nice PayPal Button Lite
+Plugin URI: http://trinitronic.com/index.php/Downloads/downloads.html
+Description: The Nice PayPal Button Lite plugin provides you with an easy PayPal payment solution. Simply add a Nice PayPal Button Lite shortcode to your post or page and a PayPal Buy Now button will be published in place of the shortcode. To change the plugin's settings visit the <a href="./options-general.php?page=nicePayPalButtonLite.php" target="_self" >settings page.</a>
+Version: 1.0
+Author: TriniTronic
+Author URI: http://trinitronic.com
+License: GPLv2 or later
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+if (!class_exists("NicePayPalButtonLite")) {
+
+  class NicePayPalButtonLite {
+  
+    var $adminOptionsName = "NicePayPalButtonLiteAdminOptions";
+  
+    function NicePayPalButtonLite() { //constructor
+       
+    }
+    
+    // Admin Panel page --------------------------------------
+    
+    // Returns an array of admin options
+    function getAdminOptions() {
+    
+      $nicePayPalButtonLiteAdminOpts = array(            
+          'currency_code'    => 'USD',
+          'country_code'     => 'US',
+          'paypal_testmode'  => 0,
+          'button_path'      => 'en_US',
+          'default_btnsize'  => '_LG',
+          'open_window'      => '_self',
+          'paypal_url'       => 'https://www.paypal.com/',
+          'paypal_testurl'   => 'https://www.sandbox.paypal.com/',
+          'paypal_email'     => '',
+          'paypal_testemail' => ''
+        );
+          
+      $niceOptions = get_option( $this->adminOptionsName );
+      
+      if ( !empty( $niceOptions ) ) {
+      
+          foreach ( $niceOptions as $k => $v )
+          
+              $nicePayPalButtonLiteAdminOpts[$k] = $v;
+              
+      }
+      
+      update_option($this->adminOptionsName, $nicePayPalButtonLiteAdminOpts);
+      
+      return $nicePayPalButtonLiteAdminOpts;
+
+    }
+    
+    function init() {
+    
+      $this->getAdminOptions();
+    
+    }
+    
+    //Prints out the admin page
+    function printAdminPage() {
+    
+      $niceOptions = $this->getAdminOptions();
+                         
+      if (isset($_POST['update_nicePayPalBUttonLiteSettings'])) {
+      
+        if (isset($_POST['currency_code'])) {
+
+          $niceOptions['currency_code'] = $_POST['currency_code'];
+          
+        }
+
+        if (isset($_POST['country_code'])) {
+
+          $niceOptions['country_code'] = $_POST['country_code'];
+          
+        }
+
+        if (isset($_POST['paypal_testmode'])) {
+
+          $niceOptions['paypal_testmode'] = $_POST['paypal_testmode'];
+          
+        }
+
+        if (isset($_POST['button_path'])) {
+
+          $niceOptions['button_path'] = $_POST['button_path'];
+          
+        }
+
+        if (isset($_POST['default_btnsize'])) {
+
+          $niceOptions['default_btnsize'] = $_POST['default_btnsize'];
+          
+        }
+
+        if (isset($_POST['open_window'])) {
+
+          $niceOptions['open_window'] = $_POST['open_window'];
+          
+        }
+
+        if (isset($_POST['paypal_url'])) {
+
+          $niceOptions['paypal_url'] = $_POST['paypal_url'];
+          
+        }
+
+        if (isset($_POST['paypal_testurl'])) {
+
+          $niceOptions['paypal_testurl'] = $_POST['paypal_testurl'];
+          
+        }
+
+        if (isset($_POST['paypal_email'])) {
+
+          $niceOptions['paypal_email'] = $_POST['paypal_email'];
+          
+        }
+
+        if (isset($_POST['paypal_testemail'])) {
+
+          $niceOptions['paypal_testemail'] = $_POST['paypal_testemail'];
+          
+        }
+          
+        update_option($this->adminOptionsName, $niceOptions);
+       
+        ?>
+            
+        <div class="updated"><p><strong><?php _e("Settings Updated.", "nicePayPalButtonLite");?></strong></p></div>
+        
+      <?php
+      } ?>
+ 
+      <div class=wrap>
+        <form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
+          <h2>Nice PayPal Button Lite</h2>
+          <h3>PayPal Account Email / Merchant Account ID</h3>
+          <p>Enter your valid PayPal account email address. Or, enter your valid Merchant Account ID, which you can find in your PayPal account profile under My Business Info. Payments will be made to the PayPal account associated with this specified email address or merchant id.</p>
+          <input type="text" size="50" name="paypal_email" id="paypal_email" value="<?php _e(apply_filters('format_to_edit',$niceOptions['paypal_email']), 'nicePayPalButtonLite'); ?>" />
+          <h3>PayPal Test Mode Email / Test Merchant Account ID</h3>		
+          <p>nter your valid PayPal sandbox test seller email address. Or, enter your valid Merchant Account ID, which you can find in your PayPal sandbox account profile under My Business Info. Test payments will be made to the sandbox account associated with this specified email address. To use this feature you must have a PayPal developer account.</p>
+          <input type="text" size="50" name="paypal_testemail" id="paypal_testemail" value="<?php _e(apply_filters('format_to_edit',$niceOptions['paypal_testemail']), 'nicePayPalButtonLite'); ?>" />	
+          <h3>PayPal Test Mode</h3>
+          <p>Select on or off to put all PayPal buttons in and out of test mode. When on is specified all transactions are sent to the PayPal sandbox. To use this feature you must have a PayPal developer account.</p>
+          <p><label for="paypal_testmode0">
+            <input type="radio" id="paypal_testmode0" name="paypal_testmode" value="0" <?php if ($niceOptions['paypal_testmode'] == 0) { _e('checked="checked"', "nicePayPalButtonLite"); }?> />off</label>&nbsp;&nbsp;&nbsp;&nbsp;
+          <label for="jform_params_paypal_testmode1">
+            <input type="radio" id="paypal_testmode1" name="paypal_testmode" value="1" <?php if ($niceOptions['paypal_testmode'] == 1) { _e('checked="checked"', "nicePayPalButtonLite"); }?> />on</label>
+          </p>
+          <h3>Currency Code</h3>		
+          <p>Valid PayPal 3-letter currency codes: Australian Dollars: AUD, Canadian Dollars: CAD, Euros: EUR, Pounds Sterling: GBP, Yen: JPY, U.S. Dollars: USD, New Zealand Dollar: NZD, Swiss Franc: CHF, Hong Kong Dollar: HKD, Singapore Dollar: SGD, Swedish Krona: SEK, Danish Krone: DKK, Polish Zloty: PLN, Norwegian Krone: NOK, Hungarian Forint: HUF, Czech Koruna: CZK, Israeli Shekel: ILS, Mexican Peso: MXN.</p>
+          <input type="text" size="3" maxlength="3" name="currency_code" id="currency_code" value="<?php _e(apply_filters('format_to_edit',$niceOptions['currency_code']), 'nicePayPalButtonLite'); ?>" />
+          <h3>PayPal Country Code</h3>		
+          <p>Enter your 2 digit country code to set the language used on the PayPal payment page. PayPal uses a two-character country code (ISO 3166). Some examples are United States: US, Great Britain: GB, France: FR, Spain: ES, Netherlands: DL, Poland: PL, German: DE. If you don't know your country code, or you can Google PayPal Country Codes.</p>
+          <input type="text" size="2" maxlength="2" name="country_code" id="country_code" value="<?php _e(apply_filters('format_to_edit',$niceOptions['country_code']), 'nicePayPalButtonLite'); ?>" />
+          <h3>Button Language Code</h3>		
+          <p>PayPal uses a 5 character code to designate the language of its buttons. For example, United States English is designated with en_US. Enter the 5 character code for the desired button language. Other code examples are Great Britain English: en_GB, French: fr_FR, Spanish: es_ES, Dutch: nl_NL, Polish: pl_PL, German: de_DE. If you don't know the code for your desired language, log into PayPal use the button creater and search the resulting HTML code for this https://www.paypal.com/en_US/i/btn/btn_buynow_LG.gif. Notice the en_US in the URL, it's the language code for the button.</p>
+          <input type="text" size="10" maxlength="10" name="button_path" id="button_path" value="<?php _e(apply_filters('format_to_edit',$niceOptions['button_path']), 'nicePayPalButtonLite'); ?>" />
+          <h3>Default Button Size</h3>
+          <p>Select the default button size. You can choose between Small and large.</p>
+          <p><label for="default_btnsize_SM">
+            <input type="radio" id="default_btnsize_SM" name="default_btnsize" value="_SM" <?php if ($niceOptions['default_btnsize'] == "_SM") { _e('checked="checked"', "nicePayPalButtonLite"); }?> />Small</label>&nbsp;&nbsp;&nbsp;&nbsp;
+          <label for="default_btnsize_LG">
+            <input type="radio" id="default_btnsize_LG" name="default_btnsize" value="_LG" <?php if ($niceOptions['default_btnsize'] == "_LG") { _e('checked="checked"', "nicePayPalButtonLite"); }?> />Large</label>
+          </p>
+          <h3>New Window Target</h3>
+          <p>Choose how you want the buyer to be sent to PayPal, either in their current browser window or a new browser window.</p>
+          <p><label for="open_window_self">
+            <input type="radio" id="open_window_self" name="open_window" value="_self" <?php if ($niceOptions['open_window'] == "_self") { _e('checked="checked"', "nicePayPalButtonLite"); }?> />Current window</label>&nbsp;&nbsp;&nbsp;&nbsp;
+          <label for="open_window_blank">
+            <input type="radio" id="open_window_blank" name="open_window" value="_blank" <?php if ($niceOptions['open_window'] == "_blank") { _e('checked="checked"', "nicePayPalButtonLite"); }?> />New window</label>
+          </p>         
+          <div class="submit">
+          <input type="submit" name="update_nicePayPalBUttonLiteSettings" value="<?php _e('Update Settings', 'nicePayPalButtonLite') ?>" /></div>
+        </form>
+      </div>
+       
+    <?php
+    }//End function printAdminPage()
+      
+
+    // -------------------------------------- End Admin Panel page
+    
+    
+    // Plugin functionality --------------------------------------
+
+    function getNicePayPalButtonLite ( $atts = '' ){
+
+      extract( shortcode_atts(
+
+      array(
+
+        'amount'	  => '',
+        'name'	    => '',
+        'sku'	      => '',
+        'shipping'	=> '',
+        'shipping2'	=> '',
+        'tax'	      => '',
+        'quantity'	=> '',
+        'weight'	  => ''
+        
+      ), $atts ));
+      
+      $opts = $this->getAdminOptions();
+      
+      $atts['currency_code']    = $opts['currency_code'];
+      $atts['country_code']     = $opts['country_code'];
+      $atts['paypal_testmode']  = $opts['paypal_testmode'];
+      $atts['button_path']      = $opts['button_path'];
+      $atts['default_btnsize']  = $opts['default_btnsize'];
+      $atts['open_window']      = $opts['open_window'];
+      $atts['paypal_email']     = $opts['paypal_email'];
+      $atts['paypal_testemail'] = $opts['paypal_testemail'];
+      $atts['paypal_url']       = 'https://www.paypal.com/';
+      $atts['paypal_testurl']   = 'https://www.sandbox.paypal.com/';
+      $atts['command']          = '_xclick';
+      $atts['url']              = $atts['paypal_testmode'] ? $atts['paypal_testurl']   : $atts['paypal_url'];
+      $atts['email']            = $atts['paypal_testmode'] ? $atts['paypal_testemail'] : $atts['paypal_email'];
+      $atts['button_image']     = $atts['url'].$atts['button_path'].'/i/btn/btn_buynow'.$atts['default_btnsize'].'.gif';
+      
+      $insert = $this->nicePayPalButtonLiteLiteBuildForm( $atts );
+      
+      return $insert;
+      
+    }  
+
+    function nicePayPalButtonLiteLiteBuildForm( $a )
+    {
+      
+      $f = '';
+      
+      if ( $a['name'] != '' && $a['command'] != '' && $a['email'] != '' && $a['url'] != '' ){
+      
+        $f .= '<form class="nicepaypalbuttonlite" action="'.$a['url'].'/cgi-bin/webscr" method="post" target="'.$a['open_window'].'">';
+        $f .= '<input type="hidden" name="business" value="'.$a['email'].'" />';
+        $f .= '<input type="hidden" name="cmd" value="'.$a['command'].'" />';
+        $f .= '<input type="hidden" name="item_name" value="'.$a['name'].'" />';
+        $f .= '<input type="hidden" name="amount" value="'.$a['amount'].'" />';
+        
+        //item number
+        if ( $a['sku'] != '' ){
+
+          $f .= '<input type="hidden" name="item_number" value="'.$a['sku'].'" />';
+
+        }
+
+        //shipping
+        if ( $a['shipping'] != '' ){
+
+          $f .= '<input type="hidden" name="shipping" value="'.$a['shipping'].'" />';
+
+        }
+
+        //shipping2
+        if ( $a['shipping2'] != '' ){
+
+          $f .= '<input type="hidden" name="shipping2" value="'.$a['shipping2'].'" />';
+
+        }
+
+        //tax
+        if ( $a['tax'] != '' ){
+                      
+        $f .= '<input type="hidden" name="tax" value="'.$a['tax'].'" />';
+
+        }
+
+        //quantity
+        if ( $a['quantity'] != '' ){
+
+        $f .= '<input type="hidden" name="quantity" value="'.$a['quantity'].'" />';
+
+        }
+
+        //weight
+        if ( $a['weight'] != '' ){
+
+         $a['weight'] = strtolower( $a['weight'] );
+         $a['weight'] = str_replace( ' ', '', $a['weight'] );
+         
+         if ( preg_match_all('/lbs/', $a['weight'], $op) || preg_match_all('/kgs/', $a['weight'], $op) ) {
+         
+           $wu  = substr( $a['weight'], -3);
+           $w   = substr( $a['weight'], 0, -3);
+           
+         }else {
+         
+           $wu  = 'lbs';
+           $w   = $a['weight'];
+         
+         }
+         
+          $f .= '<input type="hidden" name="weight" value="'.$w.'" />';
+          $f .=	'<input type="hidden" name="weight_unit" value="'.$wu.'" />';
+         
+        }
+        
+        $f .= '<input type="hidden" name="currency_code" value="'.$a['currency_code'].'" />';
+        $f .= '<input type="hidden" name="lc" value="'.$a['country_code'].'" />';
+        $f .= '<input type="hidden" name="mrb" value="YRDKF6S68Y7DS" />';
+        $f .= '<input type="image" name="submit" style="border: 0;" src="'.$a['button_image'].'" alt="PayPal - The safer, easier way to pay online" />';
+        $f .= '</form>';
+        
+      }else {
+        
+        $f .= '<div style="color: red;" >ERROR: Incomplete Nice PayPal Button Lite data!</div>';
+        
+      }
+      
+      return $f;
+
+    }
+
+  }
+
+} //End Class NicePayPalButtonLite
+
+if (class_exists("NicePayPalButtonLite")) {
+    $nice_paypalButtonLite = new NicePayPalButtonLite();
+}
+
+//Initialize the admin panel
+if ( !function_exists("nicePayPalButtonLite_ap") ) {
+
+    function nicePayPalButtonLite_ap() {
+    
+        global $nice_paypalButtonLite;
+        
+        if ( !isset($nice_paypalButtonLite) ) {
+        
+          return;
+          
+        }
+        
+        if ( function_exists('add_options_page') ) {
+        
+          add_options_page('Nice PayPal Button Lite', 'Nice PayPal Button Lite', 9, basename(__FILE__), array(&$nice_paypalButtonLite, 'printAdminPage'));
+       
+        }
+    }   
+}
+
+//Actions and Filters   
+if (isset($nice_paypalButtonLite)) {
+
+    // Init admin panel
+    add_action('admin_menu', 'nicePayPalButtonLite_ap');
+    
+    // Retrieve admin options
+    add_action('activate_nicePayPalButtonLite/nicePayPalButtonLite.php',  array(&$nice_paypalButtonLite, 'init'));
+    
+    // Adds shortcode
+    add_shortcode('nicepaypallite', array(&$nice_paypalButtonLite, 'getNicePayPalButtonLite'), 1);
+
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,218 @@
+=== Nice PayPal Button Lite ===
+Contributors: mjetpax
+Donate link: http://trinitronic.com/index.php/WordPress/wordpress-nice-paypal-button-lite.html
+Tags: paypal, button, buy now, ecommerce, shortcode, buy now button, paypal button, paypal buy now button, paypal plugin, paypal plugin for wordpress
+
+Requires at least: 3.0
+Tested up to: 3.2.1
+Stable tag: trunk
+
+Nice PayPal Button Lite gives you the power to create PayPal Buy Now buttons wherever you choose, by simply adding shortcodes to your post or page.
+
+== Description ==
+
+[Upgrade to the full version of the Nice PayPal Button plugin.](http://trinitronic.com/index.php/WordPress/wordpress-nice-paypal-button.html "Nice PayPal Button Plugin Full Version")
+
+With the Nice PayPal Button Lite plugin you get the most convenient & flexible way to add PayPal Buy Now buttons to posts and pages!
+
+Whether you want to sell a few items or a lot of items, using the traditional PayPal “Buy Now” button, the Nice PayPal Button Lite plugin makes it easy to turn any WordPress page into a point of sale. No need to monkey around with complicated ecommerce solutions, the Nice PayPal Button Lite's implementation is quick, flexible and easy to use. Simply enter the Nice PayPal Button Lite shortcode where you want your PayPal button to appear and start selling.
+
+Example
+`[nicepaypallite name="WordPress Coffee Mug" amount="12.50"]`
+
+Some cool features that you get:
+
+* Add Buy Now buttons anywhere on a post or a page.
+* Set a dollar amount or allow the payee enter it.
+* Supports PayPal Merchant ID numbers.
+* Set the item name.
+* Supports item numbers (sku numbers).
+* Supports per item shipping amounts.
+* Supports 2nd shipping amount per each additional unit.
+* Support for default button size, large/small
+* Support for PayPal window target, current or new.
+* Supports PayPal account based shipping calculations.
+* Supports PayPal account based shipping using weight in lbs or kgs.
+* Supports flat tax.
+* Set item quantity.
+* Supports PayPal country codes. 
+* Sets PayPal payment page language.
+* Supports all PayPal supported currency codes.
+* Setting for PayPal button language.
+* Test mode option, point to PayPal sandbox account.
+* Includes convenient admin options page for easy configuration.
+
+== Installation ==
+
+1. Unzip the Nice PayPal Lite plugin download
+1. Upload the entire nicePayPalButtonLite folder to your wp-content/plugins/ directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Go to Settings>>Nice PayPal Button Lite
+1. Configure the Nice PayPal Button Lite options, i.e. PayPal seller's email address, currency, country code, etc.
+1. Click the "Update Settings" button
+1. Insert Nice PayPal Button Lite shortcodes in any post or page.
+1. That's it! You're all ready to start making money!!
+
+== Frequently Asked Questions ==
+
+Will the Nice PayPal Button Lite create other buttons types, like, Add to Cart, View Cart, Donations, etc?
+
+The Nice PayPal Button Lite only supports Buy Now buttons. However, the full version of the [Nice PayPal Button plugin](http://trinitronic.com/index.php/WordPress/wordpress-nice-paypal-button.html "Nice PayPal Button Plugin Full Version") will allow you to create other button types.
+
+How many buttons can I place in a post or page?
+
+This is unlimited. You may place as many buttons as you would like in a post or page.
+
+Where can I get technical support?
+
+You can get support through our support forum [http://trinitronic.com/supportforum/index.php](http://trinitronic.com/supportforum/index.php "Nice PayPal Button Lite Support Forum").
+
+Where can I find the Nice PayPal Button Lite documentation page?
+
+You can find the Nice PayPal Button Lite Documentation here. [http://trinitronic.com/index.php/WordPress/wordpress-nice-paypal-button-lite-documentation.html](http://trinitronic.com/index.php/WordPress/wordpress-nice-paypal-button-lite-documentation.html "Nice PayPal Button Lite Documentation")
+
+== Changelog ==
+
+= 1.0 =
+* First release of the plugin
+
+== Upgrade Notice ==
+
+= 1.0 =
+* First release of the plugin
+
+== Screenshots == 
+
+== Documentation ==
+
+**Basic Usage**
+
+Usage is simple. Create a new post or page add your content to it. Wherever you want a PayPal button to appear include a shortcode like the following example. When you view your published post or page the shortcode will be automatically replaced with a corresponding PayPal button.
+
+*Shortcode Example*
+
+[nicepaypallite name="WordPress Coffee Mug" amount="12.50"]
+
+*Shortcode Syntax*
+
+[nicepaypallite attribute_name="attribute_value"]
+
+**Shortcode Attributes**
+
+The Nice PayPal Button Lite plugin provides a number of options through the inclusion of 8 shortcode attributes. These attributes are as follows.
+
+* amount
+* name
+* sku
+* shipping
+* shipping2
+* tax
+* weight
+* quantity
+
+Each attribute can be set in the shortcode by including the attribute name followed by an equals sign and the attribute value in quotes.
+
+*Attribute Example*
+
+[nicepaypallite name="WordPress Coffee Mug" amount="12.50" shipping="4.95"]
+
+**Shortcode Attribute Definitions**
+
+*name*
+
+[nicepaypallite name="Item Name"]
+
+The name of the item being sold. If omitted, payers enter their own name at the time of payment.
+
+*amount*
+
+[nicepaypallite amount="0.00"]
+
+
+The price or amount of the product, service, or contribution, not including shipping & handling, or tax. If omitted from Buy Now payers enter their own amount during checkout on the PayPal payment page. 
+
+*IMPORTANT:* Do not enter a currency symbol, just the numerical amount.
+
+*sku*
+
+[nicepaypallite sku="000"]
+
+The id number or sku number for your item. A pass-through variable for you to track product or service purchased or the contribution made. The value you specify passed back to you upon payment completion.
+
+*shipping*
+
+[nicepaypallite shipping="0.00"]
+
+The cost of shipping this item. If you specify shipping and shipping2 is not defined, this flat amount is charged regardless of the quantity of items purchased.
+
+Default – If profile-based shipping rates are configured, buyers are charged an amount according to the shipping methods they choose.
+
+*shipping2*
+
+[nicepaypallite shipping2="0.00"]
+
+The cost of shipping each additional unit of this item. If omitted and profile-based shipping rates are configured, buyers are charged an amount according to the shipping methods they choose.
+
+*tax*
+
+[nicepaypallite tax="0.00"]
+
+Transaction-based tax override variable. Set this to a flat tax to the transaction regardless of the buyer’s location. This value overrides any tax settings set in your account profile. 
+
+Default – PayPal profile tax settings, if any, apply.
+
+To use your PayPal account tax settings simply exclude the tax attribute.
+
+*quantity*
+
+[nicepaypallite quantity="0"]
+
+The number of items or item quantity included in the purchase. For example, if you wanted to sell 10 golf balls at once, you would set the item quantity to 10.
+
+*weight*
+
+[nicepaypallite weight="0.0"]
+
+The weight of the item. If PayPal account based shipping rates are configured with a basis of weight, the sum of weight values is used to calculate the shipping charges for the transaction. To enable PayPal account based shipping exclude the shipping and shipping2 attributes. After which you can optionally specify a item weight by entering a numerical (float) weight value, e.g. 1.5.
+
+You may specify pounds or kilograms by adding a suffix of lbs or kgs to your weight value. If no suffix is provided the units of weight will default to pounds.
+
+*Example*
+
+[nicepaypallite weight="1.5kgs"]
+
+**Plugin Options**
+
+The Nice PayPal Button Lite plugin allows you to configure the plugin's global settings. Configuration of the plugin is made quick and easy through the use of it's options page. You can find the options page at Admin>>Settings>>Nice PayPal Button Lite. The Global settings that are made available to you are as follows.
+
+*PayPal Account Email / Merchant Account ID*
+
+Enter your valid PayPal account email address. Or, enter your valid Merchant Account ID, which you can find in your PayPal account profile under My Business Info. Payments will be made to the PayPal account associated with this specified email address or merchant id.
+
+*PayPal Test Mode Email / Test Merchant Account ID*
+
+Enter your valid PayPal sandbox test seller email address. Or, enter your valid Merchant Account ID, which you can find in your PayPal sandbox account profile under My Business Info. Test payments will be made to the sandbox account associated with this specified email address. To use this feature you must have a PayPal developer account.
+
+*PayPal Test Mode*
+
+Select on or off to put all PayPal buttons in and out of test mode. When on is specified all transactions are sent to the PayPal sandbox. To use this feature you must have a PayPal developer account.
+
+*Currency Code*
+
+Valid PayPal 3-letter currency codes: Australian Dollars: AUD, Canadian Dollars: CAD, Euros: EUR, Pounds Sterling: GBP, Yen: JPY, U.S. Dollars: USD, New Zealand Dollar: NZD, Swiss Franc: CHF, Hong Kong Dollar: HKD, Singapore Dollar: SGD, Swedish Krona: SEK, Danish Krone: DKK, Polish Zloty: PLN, Norwegian Krone: NOK, Hungarian Forint: HUF, Czech Koruna: CZK, Israeli Shekel: ILS, Mexican Peso: MXN.
+
+*PayPal Country Code*
+
+Enter your 2 digit country code to set the language used on the PayPal payment page. PayPal uses a two-character country code (ISO 3166). Some examples are United States: US, Great Britain: GB, France: FR, Spain: ES, Netherlands: DL, Poland: PL, German: DE. If you don't know your country code, or you can Google PayPal Country Codes.
+
+*Button Language Code*
+
+PayPal uses a 5 character code to designate the language of its buttons. For example, United States English is designated with en_US. Enter the 5 character code for the desired button language. Other code examples are Great Britain English: en_GB, French: fr_FR, Spanish: es_ES, Dutch: nl_NL, Polish: pl_PL, German: de_DE. If you don't know the code for your desired language, log into PayPal use the button creater and search the resulting HTML code for this https://www.paypal.com/en_US/i/btn/btn_buynow_LG.gif. Notice the en_US in the URL, it's the language code for the button.
+
+*Default Button Size*
+
+Select the default button size. You can choose between Small and large.
+
+*New Window Target*
+
+Choose how you want the buyer to be sent to PayPal, either in their current browser window or a new browser window.
